13313704db5d20ff86f296ff6b80d293966bdccd
Pedro Momesso <pmomesso@itba.edu.ar>
2021-11-14 14:07:07 +0000

    REF: class to data class

--- app/src/main/java/com/tutv/android/domain/Genre.kt
+++ app/src/main/java/com/tutv/android/domain/Genre.kt
@@ -1,9 +1,5 @@
 package com.tutv.android.domain
 
-class Genre(var id: Int, var name: String) {
-    var seriesUri: String? = null
-    var series: List<Series>? = null
-    override fun toString(): String {
-        return name
-    }
+data class Genre(var id: Int, var name: String) {
+    override fun toString(): String = name
 }
