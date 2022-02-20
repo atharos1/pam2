337ed77ecd77992cd6abd0614e1ca8774c54a21f
Pedro Momesso <pmomesso@itba.edu.ar>
2022-02-20 12:45:35 +0000

    REF: refactor Retrofit singleton before merging PR

--- app/src/main/java/com/tutv/android/datasource/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/tutv/android/datasource/retrofit/RetrofitInstance.kt
@@ -1,6 +1,7 @@
 package com.tutv.android.datasource.retrofit
 
 import android.content.Context
+import android.util.Log
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -9,27 +10,40 @@ import com.tutv.android.datasource.retrofit.interceptor.AuthorizationInterceptor
 import com.tutv.android.datasource.retrofit.interceptor.LoginInterceptor
 import okhttp3.logging.HttpLoggingInterceptor
 
+/**
+ * Singleton lazy de la instancia de Retrofit
+ * - Dado que para inicializar la instancia es necesario acceso al Context
+ *   presentamos un unico metodo publico de acceso y hacemos de retrofit
+ *   un miembro privado. Esta es la unica diferencia con respecto a un singleton
+ *   comun y silvestre de Kotlin
+ */
 object RetrofitInstance {
+    private const val TAG = "RETROFIT INSTANCE"
+
     private const val BASE_URL = "https://tutv-pam.herokuapp.com/api/"
+    private lateinit var appContext: Context
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .client(getCustomClient(appContext!!))
+            .client(getCustomClient(appContext))
             .build()
     }
-    var appContext: Context? = null
 
-    //Todo: este metodo no va mas al momento que el DI pase a ser completamente Kotlin
+    //Todo: esta annotation no va mas al momento que DI sea completamente Kotlin!
     @JvmStatic
-    fun getRetrofitClient(appContext: Context): Retrofit? {
+    fun getRetrofitClient(appContext: Context): Retrofit {
+        if (!this::appContext.isInitialized) {
+            this.appContext = appContext
+        }
         return retrofit
     }
 
     private fun getCustomClient(appContext: Context): OkHttpClient {
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
         return OkHttpClient.Builder()
             .addInterceptor(AuthorizationInterceptor(appContext))
             .addInterceptor(LoginInterceptor(appContext))
--- app/src/main/java/com/tutv/android/di/ContainerModule.java
+++ app/src/main/java/com/tutv/android/di/ContainerModule.java
@@ -16,12 +16,13 @@ import com.tutv.android.repository.UserRepository;
 import com.tutv.android.utils.schedulers.BaseSchedulerProvider;
 import com.tutv.android.utils.schedulers.SchedulerProvider;
 
+import retrofit2.Retrofit;
+
 public class ContainerModule {
     private final Context applicationContext;
 
     public ContainerModule(Context context) {
         this.applicationContext = context.getApplicationContext();
-        RetrofitInstance.INSTANCE.setAppContext(context);
     }
 
     Context getApplicationContext() {
